471ba40dd2c30548c9a73980e4b556100862e885
adriana <atoledosantiago1@gmail.com>
2020-04-23 18:52:15 -0500

    MVP

--- index.js
+++ index.js
@@ -41,7 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+  }
 
+  eat(food) {
+      if (this.stomach.length < 10) {
+          this.stomach.push(food);
+      }
+  }
+
+  poop() {
+      this.stomach = [];
+  }
+
+  toString() {
+      return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,8 +77,26 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    return this.tank += gallons;
+  }
+  drive(distance) {
+    if (distance / this.milesPerGallon >= this.tank) {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+  }
 }
+
 
 /*
   TASK 3
@@ -75,7 +111,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(properties) {
+    this.name = properties.name;
+    this.age = properties.age;
+    this.location = properties.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,10 +135,23 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(instructorA) {
+    super(instructorA);
+    this.specialty = instructorA.specialty;
+    this.favLanguage = instructorA.favLanguage;
+    this.catchPhrase = instructorA.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 
 }
-
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -111,10 +167,24 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor (argument) {
+    super(argument);
+    this.previousBackground = argument.previousBackground;
+    this.className = argument.className;
+    this.favSubjects = argument.favSubjects;
+    
+  }
+  listSubjects() {
+    return `Loving ${this.favSubjects}`;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
-
 /*
   TASK 6
     - Write a ProjectManager class extending Instructor.
@@ -128,9 +198,23 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
 
+class ProjectManager extends Instructor {
+  constructor(argumentP) {
+      super(argumentP);
+      this.gradClassName = argumentP.gradClassName;
+      this.favInstructor = argumentP.favInstructor;
+  }
+
+  standUp(channel) {
+      return `${this.name} announces to ${channel}, @${channel} standy times!`;
+  }
+
+  debugsCode(student, subject) {
+      return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
+
 
 /*
   STRETCH PROBLEM (no tests!)
